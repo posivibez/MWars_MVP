125d7e6d18980f722bb965d634d063e2bcce69a1
Tyler Gumb <tgumb100@gmail.com>
2019-01-05 22:35:19 -0500

    Added loading bar for preload screen

--- js/scenes/loadingScene.js
+++ js/scenes/loadingScene.js
@@ -9,8 +9,37 @@ loadingScene.preload = function() {
 	//show Logo
 	let welcome = this.add.sprite(0, 0, 'welcome').setOrigin(0,0);
 
+    //progress bar background
+  let bgBar = this.add.graphics();
+  let barW = 150;
+  let barH = 30;
+  bgBar.setPosition(this.sys.game.config.width/2 - barW/2, this.sys.game.config.height/2 - barH/2);
+  bgBar.fillStyle(0xF5F5F5, 1);
+  bgBar.fillRect(0, 0, barW, barH);
+
+  //progress bar
+  let progressBar = this.add.graphics();
+  progressBar.setPosition(this.sys.game.config.width/2 - barW/2, this.sys.game.config.height/2 - barH/2);
+  //progressBar.fillStyle(0xF5F5F5, 1);
+  //progressBar.fillRect(0, 0, barW, barH);
+
+  //listen to the progress eevent
+  this.load.on('progress', function(value){
+    //clearing progress bar (so can draw again)
+    progressBar.clear();
+
+    //set style
+    progressBar.fillStyle(0x9AD98D, 1);
+
+    //draw rectangle
+    progressBar.fillRect(0, 0, value * barW, barH)
+  }, this);
+
 	//listen to the progress eevent
 	this.load.on('complete', function(){
+
+    progressBar.visible = !progressBar.visible;
+    bgBar.visible = !bgBar.visible;
 
 		let hiUnicorn = this.add.sprite(this.sys.game.config.width - 200, this.sys.game.config.height - 200, 'hiUnicorn').setScale(0.9);
 
@@ -40,6 +69,8 @@ loadingScene.preload = function() {
   	this.load.image('sadResult', 'assets/memewars/sadResult.png');
   	this.load.image('leaderboard', 'assets/memewars/TopScores.png');
     this.load.image('leaderboardBtn', 'assets/memewars/leaderboardBtn.png');
+    this.load.image('leaderboardBtn', 'assets/memewars/intro1.png');
+    this.load.image('leaderboardBtn', 'assets/memewars/intro2.png');
 
   	
   	//Memes Spritesheet TO DO figure out better way of doing this
@@ -57,9 +88,6 @@ loadingScene.create = function() {
 
 
 
-
-    this.scene.start('Game');
-/*
 //keep game on then move on
   this.time.addEvent({
       delay: 1000,
@@ -72,5 +100,5 @@ loadingScene.create = function() {
       },
       callbackScope: this
     });
-    */
+    
 };
